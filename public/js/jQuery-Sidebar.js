d7c5c64e4220caf5c41a106b2be890fff9d792de
gavin <tianyu_0723@hotmail.com>
2015-11-29 22:17:13 +0800

    导航栏

--- public/js/jQuery-Sidebar.js
+++ public/js/jQuery-Sidebar.js
@@ -1,55 +1,119 @@
 /**
  * Created by Gavin on 2015/11/27.
  */
-;(function($){
-    $.jqSidebar = function(options){
-        var setting = $.extend({
-            leftActive:false,
-            rightActive:false
-        },options);
 
-        //初始化的窗口大小
+;
+(function ($) {
+    $.jqSidebar = function (options) {
+        var setting = $.extend({
+            leftActive: false,
+            rightActive: false,
+            autoClose: true
+        }, options);
+
+        // 初始化的窗口大小
         var windowWidth = $(window).width();
 
-        //获取左右两侧的侧边栏
-        if($('.jqsb-left').length){
+        // 左右两侧侧边栏是否是激活状态 初始化为false 不是激活状态
+        var leftActive = false;
+        var rightActive = false;
+
+        // 获取左右两侧的侧边栏
+        if ($('.jqsb-left').length) {
             var $left = $('.jqsb-left');
         }
-        if($('.jqsb-right').length){
+        if ($('.jqsb-right').length) {
             var $right = $('.jqsb-right');
         }
-        if($('.jqsb-container').length){
+        if ($('.jqsb-container').length) {
             var $container = $('.jqsb-container');
         }
 
-        //初始化插件
-        function initialize(){
-            if($left && $container) $container.css('margin-left',$left.css('width'));
+        // 初始化插件
+        function init() {
+            if ($left && $container) $container.css('margin-left', $left.css('width'));
             css();
         }
 
-        initialize();
+        init();
 
-        //样式变化
-        function css(){
-            $left
+        // 样式变化
+        function css() {
+            //$left
         }
 
-        //窗口大小改变时
-        $(window).on('resize',function(){
+        // 窗口大小改变时
+        $(window).on('resize', function () {
             var resizedWindowWidth = $(window).width();
 
         });
 
-        //动画
-        function animate(){
+        // 动画
+        function animate() {
 
         }
 
-        //
-        $('.jqsb-toggle-left').on('click',function(){
+        // ---------
+        // 绑定事件
 
+        // 事件处理 防止冒泡和默认事件
+        function eventHandler(event, selector) {
+            event.stopPropagation();
+            event.preventDefault();
+        }
+
+        // 左侧导航栏开关
+        $('.jqsb-toggle-left').on('click', function (event) {
+            eventHandler(event, $(this));
+            toggle('left');
         });
+
+        // 右侧导航栏开关
+        $('.jqsb-toggle-right').on('click', function (event) {
+            eventHandler(event, $(this));
+            toggle('right');
+        });
+
+        // 点击jqsb-container的内容 关闭两侧侧边栏
+        //$container.on('click',function(event){
+        //    if(setting.autoClose && ($left || $right)){
+        //
+        //    }
+        //});
+
+        // 打开
+        function open(side) {
+            //$('.jqsb-left-sm').show();
+            $('.jqsb-left-bg').show();
+            $container.css('margin-left', $left.css('width'));
+            leftActive = true;
+        }
+
+        // 关闭
+        function close() {
+            $('.jqsb-left-bg').hide();
+            $container.css('margin-left', $left.css('width'));
+            leftActive = false;
+        }
+
+        // 开关
+        function toggle(side) {
+            if (side === 'left' && $left) {
+                if (!leftActive) {
+                    open('left');
+                } else {
+                    close();
+                }
+            }
+            if (side === 'right' && $right) {
+                if (!rightActive) {
+                    open('right');
+                } else {
+                    close();
+                }
+            }
+        }
+
     };
 
     $.jqSidebar();
